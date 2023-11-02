e9d3659d119118fad2b4d95958f13afa80cd4a1c
Sarun Kaewwongsa <k.sarun@odds.team>
2023-11-02 21:44:38 +0700

    initial saucedemo case

---
+++ cypress/cypress/e2e/saucedemo.cy.ts
@@ -0,0 +1,13 @@
+describe('Sauce Demo Scenario', () => {
+
+    //params
+
+    beforeEach(() => {
+        //For Mock Intercept
+    });
+    
+
+    it('Case : 1', () => {
+        //Start Action
+    });
+});
