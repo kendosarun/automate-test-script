2d536effb5bbdf21b35d1b0b26651873a0e08a21
Sarun Kaewwongsa <k.sarun@odds.team>
2023-11-04 15:10:32 +0700

    add test case : sauce demo scenarion on Cypress

--- cypress/cypress/e2e/saucedemo.cy.ts
+++ cypress/cypress/e2e/saucedemo.cy.ts
@@ -1,22 +1,81 @@
-import { clickLoginButton, fillPassword, fillUsername } from "../support/action-sauce-demo";
+import { clickLoginButton, fillPassword, fillUsername, verifySauceDemoPage } from "../support/action-sauce-demo";
 
 describe('Sauce Demo Scenario', () => {
 
     //params
 
-    beforeEach(() => {
-        //For Mock Intercept
+    beforeEach(() => {  
+
+    // For Mock intercept if you has.
+
     });
     
 
     it('Case : Login Standard user success', () => {
+
+        cy.visit('https://www.saucedemo.com/');
         
-        cy.visit('https://www.saucedemo.com/')
+        verifySauceDemoPage();
             
         fillUsername('standard_user');
 
         fillPassword('secret_sauce');
 
         clickLoginButton();
+
+        //Expected
+        
+        cy.get('#header_container > div.header_secondary_container > span').should('have.text', 'Products');
+        cy.get('.app_logo').should('have.text', 'Swag Labs')
+
     });
+
+    it('Case : Login with Invalid user', () => {
+
+        cy.visit('https://www.saucedemo.com/');
+
+        verifySauceDemoPage();
+            
+        fillUsername('invalid_user');
+
+        fillPassword('secret_sauce');
+
+        clickLoginButton();
+
+        //Expected
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
+
+    });
+
+    it('Case : Login with locked_out user', () => {
+
+        cy.visit('https://www.saucedemo.com/');
+
+        verifySauceDemoPage();
+            
+        fillUsername('locked_out_user');
+
+        fillPassword('secret_sauce');
+
+        clickLoginButton();
+
+        //Expected
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+
+    });
+
+
+    it('Case : Click login button without username and password ', () => {
+
+        cy.visit('https://www.saucedemo.com/');
+
+        verifySauceDemoPage();
+
+        clickLoginButton();
+
+        //Expected
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
+        
+    });
+    
 });
--- cypress/cypress/support/action-sauce-demo.ts
+++ cypress/cypress/support/action-sauce-demo.ts
@@ -15,3 +15,12 @@ export const clickLoginButton = () => {
     cy.get('[data-test="login-button"]').should('have.value', 'Login')
         .click();
 }
+
+export const verifySauceDemoPage = () => {
+
+    cy.get('#login_button_container').should('exist');
+    cy.get('#login_credentials').should('exist');
+    cy.get('[data-test="username"]').should('have.text', '')
+    cy.get('[data-test="password"]').should('have.text', '')
+    cy.get('[data-test="login-button"]').should('have.value', 'Login')
+}
