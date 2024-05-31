7609dabb7d9ebeb3d518c982f6fd38250dfe45c2
Sarun Kaewwongsa <k.sarun@odds.team>
2024-05-31 11:27:13 +0700

    add mongoDB package and test

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "cypress";
+import { configurePlugin } from 'cypress-mongodb';
 
 export default defineConfig({
+  env: {
+    mongodb: {
+        uri: 'mongodb://ftduser:ftdPassw0rd@10.7.55.55:27017/?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
+        database: 'PLUSOrderScheduling',
+        collection: 'ScheduleSalesOrder',
+    }
+},
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      configurePlugin(on);
     },
   },
 });
---
+++ cypress/cypress/e2e/test-mongo.cy.ts
@@ -0,0 +1,12 @@
+describe('qwerty', () => {
+
+    it('asdf', () => {
+        // cy.createCollection('someOtherCollection', { database: 'someDatabase', failSilently: 'true' }).then(result => {
+        //     cy.log(result);
+        //     console.log('Cypress', result);
+        // });
+        
+        // cy.dropCollection('someCollection');
+
+    });
+});
