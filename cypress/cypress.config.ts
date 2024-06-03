897d64a89f5f154c09dd2bee584577f55dbb6420
Sarun Kaewwongsa <k.sarun@odds.team>
2024-06-03 20:19:58 +0700

    install package cypress-mongodb on project

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -4,9 +4,9 @@ import { configurePlugin } from 'cypress-mongodb';
 export default defineConfig({
   env: {
     mongodb: {
-        uri: 'mongodb://ftduser:ftdPassw0rd@10.7.55.55:27017/?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
-        database: 'PLUSOrderScheduling',
-        collection: 'ScheduleSalesOrder',
+        uri: '',
+        database: '',
+        collection: '',
     }
 },
   e2e: {
--- cypress/cypress/e2e/test-mongo.cy.ts
+++ cypress/cypress/e2e/test-mongo.cy.ts
@@ -1,12 +1,53 @@
 describe('qwerty', () => {
 
     it('asdf', () => {
-        // cy.createCollection('someOtherCollection', { database: 'someDatabase', failSilently: 'true' }).then(result => {
+        // cy.createCollection('zzzCollection', { database: 'zzzz', failSilently: 'true' })
+        // .then(result => {
         //     cy.log(result);
         //     console.log('Cypress', result);
         // });
+
         
-        // cy.dropCollection('someCollection');
+        // cy.insertOne({std_id: 3, name: 'dew', status: 'active'},
+        //     {collection: 'zzzCollection', database: 'zzzz'}
+        // ).then(result => {
+        //           cy.log(result); // prints the _id of inserted document
+        // });
+
+        // cy.findOne({std_id: 2},
+        //     {collection: 'zzzCollection', database: 'zzzz'}
+        // ).then(result => {
+        //     cy.log(result);
+        //     console.log('ooo', result);
+        // });
+
+    //     cy.updateOne(
+    //         {name: 'might'},
+    //         { $set: 
+    //             {girlfriendName: 'Michel'}
+    //         },
+    //         {collection: 'zzzCollection', database: 'zzzz'}
+    //     ).then(result => {
+    //             cy.log(result);
+    //             console.log('updated', result);
+    //     });
+
+    //     cy.insertOne(
+    //         {std_id: 1, name:'Dew'},
+    //         {collection: 'xxxCollection', database: 'zzzz'}
+    //     ).then(result => {
+    //         cy.log(result);
+    //         console.log('insert', result);
+    // });
+
+
+
+        
+        // cy.dropCollection('zzzCollection')
+        // .then(result => {
+        //         cy.log(result);
+        //     console.log('Cypress', result);
+        // });
 
     });
 });
